bfeff9fbc61edd65efe774007f427aae02375543
Andre <andrepetilunalolong@gmail.com>
2025-03-12 17:48:29 +0300

    identify the most duplicated number

--- 2_display_most_duplicate_after_error.py
+++ 2_display_most_duplicate_after_error.py
@@ -1,5 +1,28 @@
-# get numbers from the user
-# identify the most duplicated number
-# stop when error occured
+
+
+
 # find the most duplicated 
 # display
+number_counts = {}
+
+while True:
+    try:
+        user_input = input("Enter a number: ") # get numbers from the user
+        number = int(user_input)
+        # count and identify the number
+        if number in number_counts:
+            number_counts[number] += 1
+        else:
+            number_counts[number] = 1
+    except ValueError:
+        print("Invalid input. Exiting program.") # stop when error occured
+        break
+
+if number_counts:
+    most_frequent = None
+    max_count = 0
+    for num in number_counts:
+        if number_counts[num] > max_count:
+            max_count = number_counts[num]
+            most_frequent = num
+    print(f"The number with the most duplicates is: {most_frequent}")
