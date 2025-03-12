6c004d1bb743c2928d9237fe3a0ebdcc47a9ab82
Andre <andrepetilunalolong@gmail.com>
2025-03-12 18:03:13 +0300

    display the highest input after an error

--- 3_display_the_higest_after_error.py
+++ 3_display_the_higest_after_error.py
@@ -1,4 +1,11 @@
-# ask the user for numbers
-# identify the numbers and check for the highest
-# stop when error occurs
-# display the highest number
+highest = None
+
+while True:
+    num = input("Enter a number: ") # ask the user for numbers
+    if not num.isdigit():
+        print(f"Highest number entered: {highest}") # print the highest when an error occured
+        print("Invalid input. Goodbye!") 
+        break
+    num = int(num) # sets the highest value
+    if highest is None or num > highest:
+        highest = num
