86286023f51d124c0cbcc279fe7f51e372ba33e9
Andre <andrepetilunalolong@gmail.com>
2025-03-12 17:31:30 +0300

    display the duplicated numbers

--- display_all_numbers_that_have_duplicate.py
+++ display_all_numbers_that_have_duplicate.py
@@ -1,3 +1,16 @@
-# ask the user for numbers
-# check for duplicate
-# display duplicated 
+
+
+
+def display_unique_numbers():
+    numbers = []
+    for i in range(10):
+            num = int(input(f"Enter value for number {i + 1}: ")) # ask the user for numbers
+            numbers.append(num)
+
+    unique_numbers = [num for num in numbers if numbers.count(num) != 1] # check for duplicate
+    if unique_numbers: # display duplicated numbers
+          print(f"Here are the list of numbers that do not have duplicates: {unique_numbers}")    
+    else:
+          print("No unique numbers entered")
+
+display_unique_numbers()
