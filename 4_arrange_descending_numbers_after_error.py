848ffd569403c0a0afa496f833053babfcceef7c
Andre <andrepetilunalolong@gmail.com>
2025-03-12 18:09:45 +0300

    arrange numbers descendingly

--- 4_arrange_descending_numbers_after_error.py
+++ 4_arrange_descending_numbers_after_error.py
@@ -1,3 +1,10 @@
-# get numbers from the user
-# stop when error occurs
-# arrange and print descendingly
+numbers = []
+
+while True:
+    num = input("Enter a number: ") # get numbers
+    if not num.isdigit(): # stop when error occcurs
+        print("Numbers entered in order from lowest to highest:")
+        print(sorted(numbers, reverse=True)) # arrange # print
+        print("Invalid input. Farewell program")
+        break
+    numbers.append(int(num))  # get the values entered
